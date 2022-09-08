e19f1db91bc88d4da3c6027b079d7c3d9a7ad469
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-07 22:59:18 -0300

    Trabajando una nueva forma las funciones

---
+++ src/es6/01-arrow-function.js
@@ -0,0 +1,12 @@
+//Esta es la forma vieja
+function square(num) {
+    return num * num;
+}
+
+//Con la nueva forma
+const squeare = (num) => {
+    return num * num;
+}
+
+//La mejor forma para trabajar, es implicito
+const square = num => num * num;
