8031241594fe52c15b319ce16f8d450d0f87a6b1
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-08 23:27:34 -0300

    Como leer un array

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -5,4 +5,9 @@ let [a, b] = fruits;
 console.log(a, fruits[1]);
 console.log(a, b);
 
-//
+//Como funciona con objetos
+//Object destruturing
+let user = { username: 'Exequiel', age: 34 };
+let { username, age } = user;
+console.log(username, age);
+console.log(username, user.age);
