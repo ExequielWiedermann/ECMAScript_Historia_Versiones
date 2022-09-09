b7d378d357cf212e6e5399c4e2d3d029c590ab3a
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-08 23:32:21 -0300

    Cambiando la forma

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -11,3 +11,14 @@ let user = { username: 'Exequiel', age: 34 };
 let { username, age } = user;
 console.log(username, age);
 console.log(username, user.age);
+
+// spread operator
+
+let person = { name: 'Exequiel', age: 34 };
+let country = 'AR';
+
+//De esta forma me entrega la forma que necesito
+let data = {...person, country };
+console.log(data);
+
+// rest
