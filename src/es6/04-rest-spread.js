8442e93c2620834d1de5f69e19f23bb2be8bfa82
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-08 23:35:26 -0300

    Cambiando la forma de hacer suma

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -22,3 +22,11 @@ let data = {...person, country };
 console.log(data);
 
 // rest
+
+function sum(num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+
+sum(1, 1, 2, 3);
