6b722a5aae6a10291582e3c6655cf5325813710f
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-08 23:16:07 -0300

    Terminando de armar el código para string

--- src/es6/03-string.js
+++ src/es6/03-string.js
@@ -7,4 +7,17 @@ console.log(epicPhrase);
 //Esto puede no estructurarse de buena forma
 //Usar Template literals
 // `` se llaman comillas francesas
+//Sintaxis mucho mejor en ES6
 let epicPhrase2 = `${hello} ${wolrd}!`;
+console.log(epicPhrase2);
+
+//Multi-line Strings
+//esto no es amigable para programar
+let lorem = 'Esto es un string \n ' + 'esto es otra línea';
+console.log(lorem);
+
+//Mejor forma en es6
+let lorem2 = `Esto es una frase epica
+la continuación de esa frase epica`;
+
+console.log(lorem2);
