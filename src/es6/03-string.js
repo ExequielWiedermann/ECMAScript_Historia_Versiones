c01c6467b2d53eb0b0119de841b86a7cdb00ed1c
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-08 23:12:24 -0300

    Usando un template para mejorar el código

---
+++ src/es6/03-string.js
@@ -0,0 +1,10 @@
+let hello = 'Hello';
+let wolrd = 'Wolrd';
+let epicPhrase = hello + ' ' + wolrd + '!';
+
+console.log(epicPhrase);
+
+//Esto puede no estructurarse de buena forma
+//Usar Template literals
+// `` se llaman comillas francesas
+let epicPhrase2 = `${hello} ${wolrd}!`;
