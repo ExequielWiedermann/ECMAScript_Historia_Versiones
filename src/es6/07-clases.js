a5b689c9cec3954c3a2822253402867af220c5aa
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-11 13:12:20 -0300

    Armando la estructura de una clase

--- src/es6/07-clases.js
+++ src/es6/07-clases.js
@@ -1,3 +1,16 @@
+//declarando
 class User {};
 
+//Instancia de una clase
 const newUser = new User();
+
+class user {
+    //métodos
+    saludo() {
+        return 'Hola';
+    }
+};
+
+const persona = new user();
+
+console.log(persona.saludo());
