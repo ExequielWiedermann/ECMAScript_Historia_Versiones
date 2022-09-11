ea8c8e7938fdec00e0568ff3ee310279ebadd202
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-11 20:09:04 -0300

    Trabajando con BigInt y Nullish

--- src/es11/01-bigint.js
+++ src/es11/01-bigint.js
@@ -1,1 +1,5 @@
-const aBigNumber = 8907212312312312313 n;
+const aBigNumber = 8907212312312312313;
+
+const anotherBigNumber = BigInt(8907212312312312313);
+console.log(aBigNumber);
+console.log(anotherBigNumber);
