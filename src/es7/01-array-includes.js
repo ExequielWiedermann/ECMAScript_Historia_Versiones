46d0dc6c2221c96366695514f7be416fba7a9353
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-11 16:04:40 -0300

    Terminando la explicación de ES7

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,10 @@
+//Con js no es necesario recorrer el array
+//Con el includes se puede hacer
+let numeros = [1, 2, 3, 4, 5, 6, ];
+
+console.log(numeros.includes(4));
+
+//Ojo con los valores, si lo dejo así no lee a Exequiel,
+//Porque esta con letra minúscula
+const list = ['Exequiel', 'Soledad', 'Tiziana'];
+console.log(list.includes('Exequiel'));
