74fe241258b3e86706d3efa3a834af6a09ab0fb9
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-11 18:02:15 -0300

    Mostrando otra forma

--- src/es8/04-trailing-commas.js
+++ src/es8/04-trailing-commas.js
@@ -1,3 +1,8 @@
 const arrayEdad = [24, 25, 26, 27];
 console.log(arrayEdad);
 console.log(arrayEdad.length);
+
+//Con esto muestra valores sin valor
+const arrayEdad2 = [24, 25, 26, 27, , , 30];
+console.log(arrayEdad2);
+console.log(arrayEdad2.length);
