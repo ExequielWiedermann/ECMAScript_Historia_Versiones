ecee9b1512be71ceecbd732d6fd1c84f3cfd70bb
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-11 18:09:50 -0300

    Creando la promise con async

--- src/es8/05-async-functions.js
+++ src/es8/05-async-functions.js
@@ -2,5 +2,12 @@ const fnAsync = () => {
     return new Promise((resolve, reject) => {
         (true) ?
         setTimeout(() => resolve('Async'), 2000): reject(new Error('Error'))
-    })
+    });
+}
+
+//Forma de como lo haciamos
+const anotherFn = async() => {
+    const someThig = await fnAsync();
+    console.log(someThig);
+    console.log('Hello');
 }
