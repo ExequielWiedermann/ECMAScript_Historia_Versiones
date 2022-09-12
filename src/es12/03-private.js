10c04dee2c1e5abad7c49667d6e8366a231d2252
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-11 22:52:10 -0300

    Armando private para una clase con ES12

---
+++ src/es12/03-private.js
@@ -0,0 +1,80 @@
+//declarando
+class User {};
+
+//Instancia de una clase
+const newUser = new User();
+
+class user {
+    //métodos
+    saludo() {
+        return 'Hola';
+    }
+};
+
+const persona = new user();
+console.log(persona.saludo());
+
+const otraCosa = new user();
+console.log(persona.saludo());
+
+//Constructor
+class user {
+    //Constructor
+    constructor() {
+        console.log('Nuevo Usuario');
+    }
+    saludo() {
+        return 'Hola'
+    }
+}
+
+const exequiel = new user();
+
+// trabajando con el this, hace referencia
+// al elemento padre que lo contiene
+
+class user {
+    constructor(name) {
+            this.name = name;
+        }
+        //métodos
+    speak() {
+        return 'Hola';
+    }
+    saludo() {
+        return `${this.speak()} ${this.name}`;
+    }
+}
+
+const alguien = new user('Tiziana');
+console.log(alguien.saludo());
+
+//Obtener elementos y guardar elementos
+
+// setters and getters
+class user {
+    //Constructor
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    //Métodos
+    speak() {
+        return 'Hola';
+    }
+    saludar() {
+        return `${this.speak()} ${this.name}`;
+    }
+
+    get uAge() {
+        return this.age;
+    }
+    set uAge(n) {
+        this.age = n;
+    }
+}
+
+const verSetGet = new user('Exequiel', 34);
+console.log(verSetGet.uAge);
+console.log(verSetGet.uAge = 20);
