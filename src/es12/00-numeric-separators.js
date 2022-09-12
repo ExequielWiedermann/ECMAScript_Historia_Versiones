a96e46bf89e8c5893cf970c79ce7f0a8d2d660f0
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-11 22:48:12 -0300

    Aprendiendo ES12 del 2021

---
+++ src/es12/00-numeric-separators.js
@@ -0,0 +1,2 @@
+const value = 100 _000_000_000;
+console.log(value);
---
+++ src/es12/01-replaceall.js
@@ -0,0 +1,3 @@
+const string = "Trabajando con JavaScritp en la versión ES12";
+const replaceString = string.replace("JavaScript", "TypeScript");
+console.log(replaceString);
